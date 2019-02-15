53a57a860c6f2e998d1a902c7dc3085ab809ddf7
AmirolAhmad <dev.amirol@gmail.com>
2019-02-15 09:49:43 +0800

    remove home link

--- app/webpacker/src/Header.js
+++ app/webpacker/src/Header.js
@@ -5,16 +5,13 @@ class Header extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-        <a className="navbar-brand mr-auto mr-lg-0" href="#">Amirol</a>
+        <Link to="/" className="navbar-brand mr-auto mr-lg-0">Amirol</Link>
         <button className="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
           <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">Home</Link>
-            </li>
             <li className="nav-item">
               <Link to="/about" className="nav-link">About</Link>
             </li>
